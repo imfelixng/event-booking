6931ee0caaf9c5b3ee8931e798321b148a1a6c13
Nguyen Quang An <ngquangan@gmail.com>
2019-01-16 16:39:40 +0700

    Add graphql and create query, mutation example

--- app.js
+++ app.js
@@ -1,15 +1,36 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.send('Welcome to my website');
-});
+app.use('/graphql', graphqlHttp({
+  schema: buildSchema(`
+    type RootQuery {
+      events: [String!]!
+    }
 
+    type RootMutation {
+      createEvent(name: String!): String
+    }
+
+    schema {
+      query: RootQuery,
+      mutation: RootMutation
+    }
+  `),
+  rootValue: {
+    events: () => ['Romantic Cooking', 'Sailing'],
+    createEvent: (args) => {
+      const eventName = args.name;
+      return eventName;
+    },
+  },
+  graphiql: true,
+}));
 app.listen(3000, () => {
   // eslint-disable-next-line no-console
   console.log('Server is running at port 3000');
