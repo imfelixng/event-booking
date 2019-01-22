4b751417e4f2bec915a5f551a5569a65ec967e71
Nguyen Quang An <ngquangan@gmail.com>
2019-01-23 02:18:41 +0700

    Add MainNavigation component and add scss for all components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,26 @@
 import React, { Component } from 'react';
+import './App.scss'
 import {BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom';
 import Auth from './pages/Auth';
 import Events from './pages/Events';
 import Bookings from './pages/Bookings';
+import MainNavigation from './components/Navigation/MainNavigation';
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <Switch>
-          <Redirect from = "/" to = "/auth" exact/>
-          <Route path = "/auth" exact component = {Auth} />
-          <Route path = "/events" exact component = {Events} />
-          <Route path = "/bookings" exact component = {Bookings} />
-        </Switch>
+        <React.Fragment>
+          <MainNavigation />
+          <main className = "main-content">
+            <Switch>
+              <Redirect from = "/" to = "/auth" exact/>
+              <Route path = "/auth" exact component = {Auth} />
+              <Route path = "/events" exact component = {Events} />
+              <Route path = "/bookings" exact component = {Bookings} />
+            </Switch>
+          </main>
+        </React.Fragment>
       </Router>
     );
   }
