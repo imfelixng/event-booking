3b7dbae6d882d899880128197e751d3d22fa2f04
Nguyen Quang An <ngquangan@gmail.com>
2019-01-27 17:21:11 +0700

    Add auth form and validate

--- frontend/src/pages/Auth.jsx
+++ frontend/src/pages/Auth.jsx
@@ -1,10 +1,69 @@
 import React, { Component } from 'react'
+import './Auth.scss';
 
 class Auth extends Component {
+
+  state = {
+    email: "",
+    password: ""
+  }
+
+  onHandleChange = (e) => {
+    let {name, value} = e.target;
+    this.setState({
+      [name]: value
+    });
+  }
+
+  onSignIn = (e) => {
+    e.preventDefault();
+    let { email, password } = this.state;
+
+    if(email.trim().length === 0 || password.trim().length === 0){
+      alert("Please enter email and password!")
+      return;
+    }
+    
+    console.log(email, password);
+  }
+
   render() {
     return (
       <React.Fragment>
-        <h1>The Auth Page</h1>
+        <form className = "auth-form" onSubmit = {this.onSignIn}>
+          <div className = "form-control">
+            <label htmlFor = "email">Email</label>
+            <input 
+              type = "email"
+              name = "email"
+              id = "email"
+              value = {this.state.email}
+              onChange = {this.onHandleChange}
+            />
+          </div>
+          <div className = "form-control">
+            <label htmlFor = "password">Password</label>
+            <input 
+              type = "password"
+              name = "password"
+              id = "password"
+              value = {this.state.password}
+              onChange = {this.onHandleChange}
+            />
+          </div>
+          <div className  ="form-actions">
+            <button
+              type = "submit"
+            >
+              Sign In
+            </button>
+            <button
+              type = "button"
+            >
+              Switch to Sign Up
+            </button>
+          </div>
+        </form>
       </React.Fragment>
     )
   }
